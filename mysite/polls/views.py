a03b17c00a3130536edc7179264df2dfc5b30d81
Mustafa Bolat <mooshcor@outlook.com>
2024-02-01 23:11:36 -0500

    added some views with lite ORM use

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,28 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse("Hello index of polls/")
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    #context dictionary is used to confirm variable/class names between template
+    #and renderer. 
+    
     
 def moosie(request):
     return HttpResponse("<h1>Moosies Page</h1> <p>fuck i love routing<p>")
     
 def detail(request, question_id):
-    response = "You're looking at question %s."
-    return HttpResponse(response % question_id)
-#join() takes all items in an iterable and joins them into one string, must have a seperator
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+    
 def results(request, question_id):
     response = "You're looking at the results of question %s"
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s. % question_id")
-    
-def what():
-    return 1
