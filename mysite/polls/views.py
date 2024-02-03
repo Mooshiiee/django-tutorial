269b4c592e129f4b9a1508ed4006d1ca62d80503
Mustafa Bolat <mooshcor@outlook.com>
2024-02-02 19:48:04 -0500

    Reapply "THIS WILL BE REVERTED, OLD VER"
    This reverts commit d1d6068ac5b42b196acea9bf84adf5dce1efd056.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.template import loader
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
-
-from .models import Choice, Question
-
 
 
 class IndexView(generic.ListView):
@@ -26,27 +27,4 @@ class ResultsView(generic.DetailView):
     template_name = "polls/results.html"
     
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        print("vote: try method has ran")
-        selected_choice = question.choice_set.get(pk=request.POST["choice"]) #access the post request
-    except (KeyError, Choice.DoesNotExist):             
-        #redisplay vote form with error message
-        print("vote: except has rn")
-        return (request, "polls/detail.html",
-            {
-            "question": question,
-            "error_message": "you didnt select a choice."
-            },
-        )
-    else:
-        print("vote: else has ran")
-        selected_choice.votes += 1
-        selected_choice.save()
-        print("vote: save has ran")
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id))) #using reverse() to get around hardcoding the URL
-        #alwasy return HttpResponseRedirect after dealing with post data to prevent double submission,
-        #in this case we redirect to the results page\
-
-def moosie(request):
-    return render(request, "polls/moosie.html")
+    return HttpResponse("You're voting on question %s. % question_id")
